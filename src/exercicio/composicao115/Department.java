5c339e769ff45ad86c47712ba5ba2e54f42c812b
thiagoalbertins <thiagoalbertins@gmail.com>
2019-11-17 02:00:58 -0300

    Create Department.java

---
+++ src/exercicio/composicao115/Department.java
@@ -0,0 +1,24 @@
+package exercicio.composicao115;
+
+public class Department {
+
+	private String name;
+
+	public Department() {
+
+	}
+
+	public Department(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
